1228e14a33bba74345ffdcc4c1a172f13e80858f
animation <763678968@qq.com>
2019-10-08 16:32:52 +0800

    工作队列 生产者Send 消费者Recv1 Recv2

--- src/main/java/com/example/rabbitmq/simple/Recv.java
+++ src/main/java/com/example/rabbitmq/simple/Recv.java
@@ -26,6 +26,7 @@ public class Recv {
 
         // 定义消费者
         DefaultConsumer consumer = new DefaultConsumer(channel) {
+            // 获取到达的消息
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String msg = new String(body, "utf-8");
